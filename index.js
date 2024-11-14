4373724f2d25664e5838290031ce743809f17ac9
heyitsGaspar <israel.chay.32@gmail.com>
2024-11-14 11:29:15 -0600

    perimiso cors

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors()); 
+app.use(cors({ origin: '*' }));
+
 app.use(express.json());
 
 app.use('/api/courses', courseRoutes);
